6461bb96b172c2a6e12b2bdcda4cf1ffe6490b2c
Adrian Prabucki <adrian.prabucki@gmail.com>
2025-04-12 16:38:22 +0200

    fix user name

--- data/ValidCredentials.py
+++ data/ValidCredentials.py
@@ -1,7 +1,7 @@
 class ValidCredentials:
     """ Class to store valid credentials for login. """
     STANDARD_USER = "standard_user"
-    PROBLEM_USER = "problems_user"
+    PROBLEM_USER = "problem_user"
     PERFORMANCE_GLITCH_USER = "performance_glitch_user"
     ERROR_USER = "error_user"
     VISUAL_USER = "visual_user"
